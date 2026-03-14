75653060856edf9e3b701e49663c86566030eab8
chenliting <987947419@qq.com>
2026-03-15 01:00:47 +0800

    feat:注释掉不需要的数据

--- zhilian-frontend/src/views/manage/ServiceProviderList.vue
+++ zhilian-frontend/src/views/manage/ServiceProviderList.vue
@@ -175,7 +175,7 @@
         <el-descriptions-item label="成立日期">{{ detailDialog.data.establishedDate || "-" }}</el-descriptions-item>
         <el-descriptions-item label="员工人数">{{ detailDialog.data.employeeCount || "-" }}</el-descriptions-item>
         <el-descriptions-item label="资质概述">{{ detailDialog.data.qualification || "-" }}</el-descriptions-item>
-        <el-descriptions-item label="年收入(万元)">{{ detailDialog.data.annualRevenue || "-" }}</el-descriptions-item>
+        <!-- <el-descriptions-item label="年收入(万元)">{{ detailDialog.data.annualRevenue || "-" }}</el-descriptions-item> -->
         <el-descriptions-item label="企业logo" :span="2">
           <el-image
             v-if="detailDialog.data.logo"
