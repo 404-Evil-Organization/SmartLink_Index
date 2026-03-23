1bcfca69b213358a24c125f2bbee95015b3cb576
chenliting <987947419@qq.com>
2026-03-23 18:47:29 +0800

    Update BasicLayout.vue

--- zhilian-frontend/src/layouts/BasicLayout.vue
+++ zhilian-frontend/src/layouts/BasicLayout.vue
@@ -106,13 +106,13 @@
             </el-icon>
             <span>标签管理</span>
           </el-menu-item>
-          <el-menu-item index="/admin/enterprise-audit">
-            <el-icon><Checked /></el-icon>
-            <span>企业审核</span>
-          </el-menu-item>
           <el-menu-item index="/admin/user">
             <el-icon><Avatar /></el-icon>
             <span>用户管理</span>
+          </el-menu-item>
+          <el-menu-item index="/admin/enterprise-audit">
+            <el-icon><Checked /></el-icon>
+            <span>企业审核</span>
           </el-menu-item>
         </el-sub-menu>
         <!-- 后续可继续添加其他菜单项 -->
